9967008f2696d5dfd9d0ba9e3f61f52e1e24ac90
GwangHyeon Kim <56045944+g00hyun@users.noreply.github.com>
2024-01-21 21:12:13 +0900

    [level 2] Title: H-Index, Time: 0.01 ms, Memory: 3.71 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.cpp
+++ 프로그래머스/2/42747. H－Index/H－Index.cpp
@@ -9,16 +9,11 @@ int solution(vector<int> citations) {
     int answer = 0;
     
     sort(citations.begin(), citations.end(), greater<>());
-    
-    for(auto i : citations)
-        cout << i << ' ';
-    
-    int maxhidx = 1;
     for(int i = 0; i<citations.size(); i++) {
         
         if(i+1 <= citations[i]) {
-            // maxhidx = hidx;
-            answer++;
+            answer = i+1;
+            // answer++;
         }
     }
     
