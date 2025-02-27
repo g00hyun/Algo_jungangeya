716ae58a8f418941a1d84e2f6585c505d03d5556
GWANGHYEON KIM <56045944+g00hyun@users.noreply.github.com>
2025-02-27 16:58:21 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -1,23 +1,9 @@
 function solution(s) {
-    let answer = [];
     const converter = new Map();
     converter.set('zero', 0).set('one', 1).set('two', 2).set('three',3).set('four',4).set('five',5).set('six',6).set('seven',7).set('eight',8).set('nine',9);
     
-    let start = 0, end = 0;
-    for(let i = 0; i<s.length; i++) {
-        end = i
-        if(Number.isInteger(Number(s[end]))) {
-            answer.push(Number(s[end]))
-            start = end + 1
-        }
-        else {
-            const word = s.slice(start, end + 1)
-            if(converter.has(word)) {
-                answer.push(converter.get(word))
-                start = end + 1
-            }
-        }
-        
+    for(let [k,v] of converter) {
+        s = s.replaceAll(k, v)
     }
-    return Number(answer.join(''));
+    return Number(s);
 }
